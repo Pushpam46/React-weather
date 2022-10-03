2400bf7817ffdb4734446f6a487fb0d568f1d66e
Pushpam Chodankar <chodankarpushpam46@gmail.com>
2022-10-03 15:04:31 +0530

    First changes

--- src/App.js
+++ src/App.js
@@ -19,6 +19,16 @@ function App() {
     }
   }
 
+  
+  let unix_timestamp1 = data.sys?data.sys.sunrise:null
+  let unix_timestamp2 = data.sys?data.sys.sunset:null
+
+  let date1 = new Date(unix_timestamp1 * 1000);
+  let date2 = new Date(unix_timestamp2 * 1000);
+
+  let sunrise = date1.toLocaleTimeString("en-US");
+  let sunset = date2.toLocaleTimeString("en-US");
+
   return (
     <div className="App">
       <div className='search'>
@@ -28,27 +38,32 @@ function App() {
           value={location}
           placeholder='Enter City Name'/>
       </div>
-
+      {
+          data.name != undefined && 
         <div className='container'>
 
       
           <div className='top'>
             <div className='location'>
-              <p>{data.name}</p>
+              <p>{data.name} {data.sys?data.sys.country:null}</p>
             </div>
             <div className='temp'>
-              {data.main?<h1>{data.main.temp.toFixed()}&#8457;</h1>: null}
+              {data.main?<h1>{((data.main.temp - 32) * 5 / 9).toFixed()}&#8451;</h1>: null}
+            </div>
+            <div className='riseSet'>
+              <h2>Sunrise {sunrise}</h2>
+              <h2>Sunset {sunset}</h2>
             </div>
             <div className='description'>
-            {data.weather?<p>{data.weather[0].main}</p>: null}
+              {data.weather?<p>{data.weather[0].description}</p>: null}
             </div>
           </div>
 
-          {
-          data.name != undefined &&          
+            
+  
             <div className='bottom'>
               <div className='feels'>
-              {data.main?<p className='bold'>{data.main.feels_like.toFixed()}&#8457;</p>: null}
+              {data.main?<p className='bold'>{((data.main.feels_like- 32) * 5 / 9).toFixed()}&#8451;</p>: null}
                 <p>Feels Like</p>
               </div>
               <div className='humidity'>
@@ -60,9 +75,10 @@ function App() {
                 <p>Wind Speed</p>
               </div>
             </div>
-          }
+          
 
         </div>
+      }
     </div>
   );
 }
